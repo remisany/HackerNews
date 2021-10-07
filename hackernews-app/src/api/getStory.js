a6b2d57f218dae241cd2171a20bca115f2611020
remisany <remi.d.sany@gmail.com>
2021-10-07 15:51:19 +0200

    calls are now in homepage

--- hackernews-app/src/api/getStory.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-function getStory (storyId) {
-    return axios
-        .get(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
-        .then(response => response.data)
-}
-
-export default getStory
--- hackernews-app/src/api/getStoryIds.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-
-function getStoryIds () {
-    return axios
-        .get("https://hacker-news.firebaseio.com/v0/topstories.json")
-        .then(response => response.data)
-}
-
-export default getStoryIds
